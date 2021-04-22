a9049f400a4f84fbf83836b1fe5d0781b1f66f2e
dprily@gmail.com <r0zcJj13>
2021-04-22 13:08:47 +0300

    Add debugbar

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -31,11 +31,11 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <div class="brand-link">
                 <img src="/admin_panel/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Админ-панель</span>
-            </a>
+            </div>
 
             <!-- Sidebar -->
             <div class="sidebar">
